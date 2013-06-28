1808d5da9f6442c91fc35f70905ea40581dc48fa
vindex1986 <vindex1986@gmail.com>
2013-06-28 11:20:21 +0800

    增加計步器,以一軸3點凸偵測X(i+1)-X(i)、X(i+2)-X(i+1)

--- Motion/src/com/vindex/motion/MotionActivity.java
+++ Motion/src/com/vindex/motion/MotionActivity.java
@@ -19,7 +19,6 @@ import java.io.IOException;
 import android.view.View;
 import android.app.Service;
 import android.os.Vibrator;
-
  
 public class MotionActivity extends Activity {		//繼承Activity類別
  
@@ -27,10 +26,18 @@ public class MotionActivity extends Activity {		//繼承Activity類別
 	boolean accelerometerPresent;
 	Sensor accelerometerSensor, gyroscpeSensor;
 //	Sensor gyroscopeSensor;
-	TextView textX, textY, textZ, axisX, axisY, axisZ, textAcc;
+	TextView textX, textY, textZ, axisX, axisY, axisZ, textAcc, textstep;
 	private int flag = 0;
 	private int sleep = 1;
-	float Acc = 0, tmpA, tmpB, tmpC, tmpD;
+	float Acc = 0, tmpA = 0, Max = 0;
+	int step = 0;
+	boolean stable = false,  flagFirst = true, deltaFirst = true;
+	float data[] = {0,0,0};
+	float delta[] = {(float)0.7, (float)-0.7};
+	float deltatmp[] = {0, 0};
+	float avg = 0;
+	long startTime;
+	long consumingTime;
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -38,6 +45,7 @@ public class MotionActivity extends Activity {		//繼承Activity類別
 		super.onCreate(savedInstanceState);			//呼叫Activity父類別的onCreate()方法
 //		setContentView(R.layout.activity_motion);	//呼叫版面配置檔案
 		super.setContentView(R.layout.activity_motion);
+		
 		
 		Button Start = (Button)findViewById(R.id.button1);	//建立並取得Button
 		Button Stop = (Button)findViewById(R.id.button2);
@@ -48,44 +56,46 @@ public class MotionActivity extends Activity {		//繼承Activity類別
 		axisY = (TextView)findViewById(R.id.axisY);
 		axisZ = (TextView)findViewById(R.id.axisZ);
 		textAcc = (TextView)findViewById(R.id.textAcc);
-		
+		textstep = (TextView)findViewById(R.id.textstep);
 		
 		sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
 		accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-		gyroscpeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE); 
+		gyroscpeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
 		
 		Start.setOnClickListener(new Button.OnClickListener(){	//定義監聽Start
 			@Override
 			public void onClick(View arg0) {
 			// TODO Auto-generated method stub
 				flag = 1;
-			}});       
+			}});
 		 
-		Stop.setOnClickListener(new Button.OnClickListener(){
+		Stop.setOnClickListener(new Button.OnClickListener(){	//stop
 			@Override
 			public void onClick(View arg0) {
 		    // TODO Auto-generated method stub
 				flag = 0;
 				sleep = 1;
-			}});        
+				step = 0;
+				stable = false;
+				flagFirst = true;
+				deltaFirst = true;
+			}});
 	}
   
 	@Override
 	protected void onResume() {
 	// TODO Auto-generated method stub
 		super.onResume();
-
-			sensorManager.registerListener(accelerometerListener, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
-			sensorManager.registerListener(accelerometerListener, gyroscpeSensor, SensorManager.SENSOR_DELAY_NORMAL);
-			Toast.makeText(this, "Register accelerometerListener", Toast.LENGTH_LONG).show();
-
+		sensorManager.registerListener(accelerometerListener, accelerometerSensor, SensorManager.SENSOR_DELAY_FASTEST);
+		sensorManager.registerListener(accelerometerListener, gyroscpeSensor, SensorManager.SENSOR_DELAY_NORMAL);
+		Toast.makeText(this, "Register accelerometerListener", Toast.LENGTH_LONG).show();
 	}
  
 	@Override
 	protected void onStop() {
 	// TODO Auto-generated method stub
 		super.onStop();
- 
+
 		if(accelerometerPresent){
 			sensorManager.unregisterListener(accelerometerListener);
 			Toast.makeText(this, "Unregister accelerometerListener", Toast.LENGTH_LONG).show();
@@ -102,57 +112,107 @@ public class MotionActivity extends Activity {		//繼承Activity類別
 		@Override	//當sensor有變動時就會執行
 		public void onSensorChanged(SensorEvent event) {
 		// TODO Auto-generated method stub
-	 
+			consumingTime = (System.nanoTime() - startTime);
+			startTime = System.nanoTime();
+			
 			Vibrator myVibrator = (Vibrator) getApplication().getSystemService(Service.VIBRATOR_SERVICE);
 			
 			try{
 				FileWriter fw = new FileWriter(getString(R.string._sdcard_accelerometerSensor_txt), true);
 				BufferedWriter bw = new BufferedWriter(fw); //將BufferedWeiter與FileWrite物件做連結
   
-				if(flag == 1){
+				if(flag == 1){	//check ButtonClick
 					
 					try{
 						if(sleep == 1){
 							Thread.sleep(2000);
+							myVibrator.vibrate(200);
 							sleep = 0;
 						}
-					} catch(InterruptedException e){
+					} 
+					catch(InterruptedException e){
 					}
 					
 					if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-						textX.setText("X: " + String.valueOf(event.values[0]));
-						textY.setText("Y: " + String.valueOf(event.values[1]));
-						textZ.setText("Z: " + String.valueOf(event.values[2]));
+						textX.setText("X: " + String.valueOf(event.values[0]));	//X
+						textY.setText("Y: " + String.valueOf(event.values[1]));	//Y
+						textZ.setText("Z: " + String.valueOf(event.values[2]));	//Z
+					
+/*						while(!stable){
+							tmpA = (float) Math.abs(Math.pow((float)event.values[0], 2) +
+									Math.pow((float)event.values[1], 2) +
+									Math.pow((float)event.values[2], 2));// -9.75*9.75
+							Acc = (float) java.lang.Math.sqrt(tmpA);
+							textAcc.setText("Acc " + String.valueOf(Acc));
+			
+							if ((Acc < 10)&&(Acc > 9)){							
+								stable = true;
+								myVibrator.vibrate(200);
+							}							
+						}
+*/					
 						
-						tmpA = (float)event.values[0];
-						tmpB = (float)event.values[1];
-						tmpC = (float)event.values[2];
-						
-						tmpD = (float) Math.abs(tmpA*tmpA +tmpB*tmpB +tmpC*tmpC);// -9.75*9.75
-						Acc = (float) java.lang.Math.sqrt(tmpD);
+						tmpA = (float) Math.abs(Math.pow((float)event.values[0], 2) +
+												Math.pow((float)event.values[1], 2) +
+												Math.pow((float)event.values[2], 2));// -9.75*9.75
+						Acc = (float) java.lang.Math.sqrt(tmpA);
 						textAcc.setText("Acc " + String.valueOf(Acc));
 						
-						if (Acc >= 17.64 ){
-							myVibrator.vibrate( 1500 );
+						if (Acc >= 20 ){
+//							myVibrator.vibrate(1500);
 						}
 						
+						data[0] = data[1];
+						data[1] = data[2];
+						data[2] =(float)event.values[1]; 				
+						
+						if (flagFirst){
+							avg = (float)event.values[1];
+							flagFirst = false;
+						}
+						
+						avg = (avg + (float)event.values[1])/2;
+						
+						if (((data[1] - data[0]) > delta[0] )&& ((data[2] - data[1]) < delta[1])&&
+							 (data[0] < avg)&& (data[1] > avg )&& (data[2] < avg)){
+							
+								step++;
+						
+/*								if (deltaFirst){
+									deltatmp[0] = (data[1] - data[0]);
+									deltatmp[1] = (data[2] - data[1]);
+									deltaFirst = false;
+								}
+								
+								deltatmp[0] = ((data[1] - data[0]) + deltatmp[0])/2;
+								deltatmp[1] = ((data[2] - data[1]) + deltatmp[1])/2;
+								
+								if (step > 5){
+									delta[0] = deltatmp[0];
+									delta[1] = deltatmp[1];
+								}
+*/								
+						}
+						
+						textstep.setText("Step: " + String.valueOf(step*2));
+
+bw.write(String.valueOf(consumingTime/1000) + ":");
 //bw.write(String.valueOf(event.values[0]) + ",");
 						//bw.newLine();
-//bw.write(String.valueOf(event.values[1]) + ",");
+bw.write(String.valueOf(event.values[1]) + ",");
 						//bw.newLine();
 //bw.write(String.valueOf(event.values[2] + ","));
 						//bw.newLine();
 						bw.write(String.valueOf(Acc));					
 						bw.newLine();
 						bw.close();	
-					}
+					}}
 				}
-  
-			}catch(IOException e){ 
+			catch(IOException e){ 
 				e.printStackTrace();	
-			}					
+			}				
 			
-
+/*
 			try{
 				FileWriter fw = new FileWriter(getString(R.string._sdcard_gyroscpeSensor_txt), true);
 				BufferedWriter bw = new BufferedWriter(fw); //將BufferedWeiter與FileWrite物件做連結
@@ -189,6 +249,6 @@ public class MotionActivity extends Activity {		//繼承Activity類別
 			}catch(IOException e){ 
 				e.printStackTrace();	
 			}						
-     
+*/   
 		}};
 }
